76d8e8088dcc37e075a80df9d1abf43693d5c358
Huzbi <76440095+Huzbi-crypto@users.noreply.github.com>
2023-03-27 16:59:43 +0500

    Update currency_rates_pkr.cpp
    This time, I made a class that performs the operations and encapsulates the data members and abstracts the scary code that is serving the operations.

--- Sem-2/Currency Rates Project/currency_rates_pkr.cpp
+++ Sem-2/Currency Rates Project/currency_rates_pkr.cpp
@@ -1,55 +1,110 @@
-// Writing a program that uses the api from Open Exchange Rates and displays the current exchange rates for the currencies against Pakistan Rupees and saves them in a
-// text file.
+// Writing a program that uses cURL to get the current exchange rates for the currencies against Pakistan Rupees and saves them in a text file.
 
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <curl/curl.h>
 
 using namespace std;
 
-// This function will be called by cURL when new data is received
-size_t writeCallback(char* buf, size_t size, size_t nmemb, void* up)
-{
-    // Cast the 'up' argument to a ofstream* pointer
-    ofstream* file = (ofstream*) up;
+// This class will be used to get the exchange rates for the currencies against Pakistan Rupees and save them in a text file.
+class CurrencyConverter {
+public:
 
-    // Write the received data to the file
-    file->write(buf, size * nmemb);
+    // Constructor that takes the API key as an argument
+    CurrencyConverter(const string& api_key) : api_key_(api_key) {}
 
-    // Return the number of bytes written
-    return size * nmemb;
-}
+    // Get the exchange rates for the currencies against Pakistan Rupees
+    bool get_exchange_rates(const string& base_currency, const vector<string>& currencies) {
 
-int main()
-{
-    // Initialize cURL
-    CURL* curl = curl_easy_init();
+        // Construct the URL to get the exchange rates
+        string url = "https://open.er-api.com/v6/latest/" + base_currency + "?apikey=" + api_key_;
 
-    // Set the API endpoint URL and parameters
-    // Replace YOUR_API_KEY with your API key that you get after signing up at Open Exchange Rates
-    string url = "https://openexchangerates.org/api/latest.json?app_id=YOUR_API_KEY&base=PKR&symbols=USD,EUR,GBP,JPY,CAD";
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        // Add the currencies to the URL as a comma separated list
+        url += "&symbols=";
+        for (const auto& currency : currencies) {
+            url += currency + ",";
+        }
 
-    // Create a file to save the response
-    ofstream file("currency_rates.txt");
+        // Remove the last comma from the URL
+        url.pop_back(); 
 
-    // Set the writeCallback function to be called when data is received
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
+        // Initialize cURL
+        CURL* curl = curl_easy_init();
 
-    // Set the 'up' argument to be the file stream
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &file);
+        // Create a string to save the response
+        string readBuffer;
 
-    // Perform the cURL request
-    CURLcode res = curl_easy_perform(curl);
+        // Set the writeCallback function to be called when data is received and pass the string to it to be appended to
+        if (curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
 
-    // Check for errors
-    if (res != CURLE_OK) {
-        cout << "cURL error: " << curl_easy_strerror(res) << endl;
+            // Perform the cURL request
+            CURLcode res = curl_easy_perform(curl);
+
+            // Check for errors
+            if (res != CURLE_OK) {
+                cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << endl;
+                return false;
+            }
+
+            // Clean up cURL
+            curl_easy_cleanup(curl);
+        } else {
+            cerr << "Failed to initialize curl" << endl;
+            return false;
+        }
+
+        // Save the response to the exchange_rates_ member variable
+        exchange_rates_ = readBuffer;
+        return true;
     }
 
-    // Clean up cURL and close the file stream
-    curl_easy_cleanup(curl);
-    file.close();
+    // Save the exchange rates to a text file
+    bool save_exchange_rates(const string& filename) {
 
+        // Open the file for writing
+        ofstream outputFile(filename);
+
+        // Write the exchange rates to the file
+        if (outputFile.is_open()) {
+            outputFile << exchange_rates_ << endl;
+            outputFile.close();
+            cout << "Exchange rates saved to " << filename << endl;
+            return true;
+        } else {
+            cerr << "Failed to open or create " << filename << endl;
+            return false;
+        }
+    }
+
+private:
+
+    // This function will be called by cURL when new data is received
+    static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+        ((string*)userp)->append((char*)contents, size * nmemb);
+        return size * nmemb;
+    }
+
+    // The API key and the exchange rates
+    string api_key_;
+    string exchange_rates_;
+};
+
+int main() {
+
+    // Initialize the CurrencyConverter object with the API key
+    CurrencyConverter converter("YOUR_API_KEY");
+
+    // In the vector below, add the currencies you want to get the exchange rates for
+    vector<string> currencies = {"USD", "EUR", "GBP", "AUD", "JPY"};
+
+    // Get the exchange rates and save them to a text file
+    if (converter.get_exchange_rates("PKR", currencies)) {
+        converter.save_exchange_rates("exchange_rates.txt");
+    }
+    
     return 0;
 }
